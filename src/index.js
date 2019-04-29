e9a06fd83bc7f973b31790b3fa5cead22f63e30f
James Hickey <jamesmichaelhickey@gmail.com>
2019-04-29 16:08:36 -0300

    working...?

--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ const readline = require('readline');
 const glob = require('glob');
 const marked = require('marked');
 const staticaContentRegex = /[<][!][-][-][@]Content[(][)][-][-][>]/;
+const distFolder = "www/";
+const assetsFolderGlob = "assets/**/**.*";
+const markdownFilesGlob = '**/**.md';
+const distFilesGlob = "www/**/**.*";
+const templateFile = "_template.html";
+
+const getDirectoryFromFile = file => {
+    const array = file.split(/[/\\]/);
+    return array.slice(0, array.length - 1).join("/");
+}
+
+const tryCreateDir = dir => {
+    if(!fs.existsSync(dir))
+        fs.mkdirSync(dir); 
+}           
+
+const throwErr = err => {
+    if(err)
+        throw err;
+}
+
 
 const processMarkdownFiles = async (err, markdownFiles) => {
     if(err) throw err;
 
+    markdownFiles.sort((a, b) => a.length >= b.length);
+
     for (const file of markdownFiles) {            
-        const templateLines = readline.createInterface({ input: fs.createReadStream("./_template.html") });
+        const templateLines = readline.createInterface({ input: fs.createReadStream(templateFile) });
         let newContentArray = [];
         templateLines.on('line', line => {
             if(staticaContentRegex.test(line)) {
@@ -19,19 +42,31 @@ const processMarkdownFiles = async (err, markdownFiles) => {
             newContentArray.push(line);
         });
         templateLines.on('close', () => {
-            const fileWithoutExtension = file.replace(/[.]md$/, '');
-            const targetDir = "./dir/" + fileWithoutExtension.replace(/[\\/][^\\/].*$/, '')
+            const filename = file.replace(/[.]md$/, '');
+            const targetDir = getDirectoryFromFile(distFolder + file);
 
-            if(!fs.existsSync(targetDir)) {
-                fs.mkdirSync(targetDir);            
-            }
-            const outputFilename = "./dist/" + fileWithoutExtension + ".html";
-            fs.writeFile(outputFilename, );
+            tryCreateDir(targetDir);
+
+            const outputFilename = distFolder + filename + ".html";
+            fs.writeFile(outputFilename, newContentArray.join(""), throwErr);
         });
     }
-
 }
 
-glob('**/*.md', {
-    ignore: ['./assets/**.*']
+const copyFilesToOutputFolder = async (err, files) => {
+    files.sort((a, b) => a.length >= b.length);
+
+    for(const file of files) {
+        const dir = getDirectoryFromFile(file);
+        tryCreateDir(distFolder + dir);
+        fs.copyFile(file, distFolder + file, throwErr);
+    }
+}
+
+glob(markdownFilesGlob, {
+    ignore: [assetsFolderGlob]
 }, processMarkdownFiles);
+
+glob('**/**.*', { ignore: [templateFile, distFilesGlob, markdownFilesGlob, assetsFolderGlob] }, copyFilesToOutputFolder);
+
+glob(assetsFolderGlob, copyFilesToOutputFolder);
